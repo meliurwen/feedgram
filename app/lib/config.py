c87bcc9464e9af665be57e7f777589da52f531a7
territory91 <terribile_91@hotmail.it>
2020-04-12 20:13:47 +0200

    Fix test case, disable pylint R1705, changed protected to private in config.py

--- app/lib/config.py
+++ app/lib/config.py
@@ -14,60 +14,70 @@ DEFAULT_CONFIG = {"BOT": {"databasefilepath": "socialFeedgram.sqlite3"},
 
 # module_logger = logging.getLogger('telegram_bot.config')
 
+# Structure Config
+# [BOT]
+# databasefilepath
+# [API]
+# telegramkey": ""
+# "youtubev3key": ""
+
 
 class Config:
-    # [BOT]
-    # databasefilepath
-    # [API]
-    # telegramkey": ""
-    # "youtubev3key": ""
+    # Private:
+    #   logger
+    #   file_config
+    #   state
+    #   cofigurazione_dict
+    # Public:
+    #   dictionary
+    #   status
 
     def __init__(self, fileConfig):
 
-        self.logger = logging.getLogger('telegram_bot.Config')
-        self.logger.info('Creating an instance of Config')
+        self.__logger = logging.getLogger('telegram_bot.Config')
+        self.__logger.info('Creating an instance of Config')
 
-        self.logger.info("################### CONFIGURATION LOADING #########################")
+        self.__logger.info("################### CONFIGURATION LOADING #########################")
 
-        self._file_config = fileConfig
-        self._state = 1
+        self.__file_config = fileConfig
+        self.__state = 1
 
-        self.logger.info("Controllo se esiste già il file di configurazione %s ...", fileConfig)
-        if os.path.exists(self._file_config):
-            self.logger.info("Il file %s esiste.", fileConfig)
+        self.__logger.info("Controllo se esiste già il file di configurazione %s ...", fileConfig)
+        if os.path.exists(self.__file_config):
+            self.__logger.info("Il file %s esiste.", fileConfig)
             self.load_config()
         else:
-            self.logger.info("################### CONFIGURATION CREATION #########################")
+            self.__logger.info("################### CONFIGURATION CREATION #########################")
             self.__cofigurazione_dict = DEFAULT_CONFIG
-            self._save_config()
+            self.save_config()
             self.load_config()
 
-        self.logger.info("################### CONFIGURATION COMPLETE #########################")
+        self.__logger.info("################### CONFIGURATION COMPLETE #########################")
 
     def load_config(self):
-        self.logger.info("################### CONFIGURATION CONFIGURAZIONE #########################")
+        self.__logger.info("################### CONFIGURATION CONFIGURAZIONE #########################")
         configurazione = configparser.ConfigParser()
         try:
-            configurazione.read(self._file_config)
+            configurazione.read(self.__file_config)
         except configparser.DuplicateOptionError:
-            self.logger.warning("Delle chiavi sono duplicate")
-            self._state = -2
-            # return -2
+            self.__logger.warning("Delle chiavi sono duplicate")
+            self.__state = -2
+            return
         except configparser.DuplicateSectionError:
-            self.logger.warning("Delle sezioni sono duplicate")
-            self._state = -3
-            # return -3
+            self.__logger.warning("Delle sezioni sono duplicate")
+            self.__state = -3
+            return
         # Converto l'oggetto configurazione in un dizionario
-        self.__cofigurazione_dict = self._as_dict(configurazione)
-        self.logger.info("Configurazione caricata.")
-        if self._check_config:
-            self.logger.info("Controllo conclusosi con successo.")
+        self.__cofigurazione_dict = self.__as_dict(configurazione)
+        self.__logger.info("Configurazione caricata.")
+        if self.__check_config:
+            self.__logger.info("Controllo conclusosi con successo.")
             # self._state = -4
             # return -4
 
-    def _save_config(self):
-        self.logger.info("################### SALVATAGGIO CONFIGURAZIONE #########################")
-        self._write_config(self._file_config, self.__cofigurazione_dict)
+    def save_config(self):
+        self.__logger.info("################### SALVATAGGIO CONFIGURAZIONE #########################")
+        self.__write_config(self.__file_config, self.__cofigurazione_dict)
 
     @property
     def dictionary(self):
@@ -75,11 +85,11 @@ class Config:
 
     @property
     def status(self):
-        return self._state
+        return self.__state
 
     # private function
 
-    def _write_config(self, file_config, config):
+    def __write_config(self, file_config, config):
         try:
             with open(file_config, 'w') as file_config_obj:
                 configurazione = configparser.ConfigParser()
@@ -87,46 +97,47 @@ class Config:
                     configurazione[sezione] = config[sezione]
 
                 configurazione.write(file_config_obj)
-                self.logger.info("File config impostato con sucesso")
+                self.__logger.info("File config impostato con sucesso")
         except OSError:
-            self.logger.warning(
+            self.__logger.warning(
                 "Qualcosa è andato storto. Impossibile creare il file di configurazione.")
 
     @property
-    def _check_config(self):
-        self.logger.info("################### CONTROLLO CONFIGURAZIONE#########################")
-        self.logger.info("Controllo che ci siano esattamente tutte le sezioni...")
+    def __check_config(self):
+        self.__logger.info("################### CONTROLLO CONFIGURAZIONE#########################")
+        self.__logger.info("Controllo che ci siano esattamente tutte le sezioni...")
         if not set(self.__cofigurazione_dict.keys()) == set(DEFAULT_CONFIG.keys()):
-            self.logger.warning("NON ci sono ESATTAMENTE tutte le sezioni.")
-            self._state = -5
+            self.__logger.warning("NON ci sono ESATTAMENTE tutte le sezioni.")
+            self.__state = -5
             return False
-        self.logger.info("Ci sono tutte le sezioni.")
+        self.__logger.info("Ci sono tutte le sezioni.")
 
-        self.logger.info("Controllo che in ogni sezione ci siano esattamente tutte le chiavi...")
+        self.__logger.info("Controllo che in ogni sezione ci siano esattamente tutte le chiavi...")
 
         for key, _ in DEFAULT_CONFIG.items():
             if not set(self.__cofigurazione_dict[key].keys()) == set(DEFAULT_CONFIG[key].keys()):
-                self.logger.warning("Nella sezione %s NON ci sono ESATTAMENTE tutte le chiavi!", str(key))
-                self.logger.warning("Miss the key(s): %s ", ''.join(list(set(self.__cofigurazione_dict[key].keys()) - set(DEFAULT_CONFIG[key].keys()))))
-                self._state = -6
+                self.__logger.warning("Nella sezione %s NON ci sono ESATTAMENTE tutte le chiavi!", str(key))
+                self.__logger.warning("Miss the key(s): %s ", ''.join(list(set(self.__cofigurazione_dict[key].keys()) - set(DEFAULT_CONFIG[key].keys()))))
+                self.__state = -6
+                return False
             else:
                 for key_section, value_section in self.__cofigurazione_dict[key].items():
                     if not str(value_section):
-                        self.logger.warning("Nella sezione %s l'opzione %s è VUOTA!", str(key), str(key_section))
-                        self._state = -7
-            return False
+                        self.__logger.warning("Nella sezione %s l'opzione %s è VUOTA!", str(key), str(key_section))
+                        self.__state = -7
+                        return False
 
-        self.logger.info("Ci sono tutte le chiavi in ogni sezione.")
+        self.__logger.info("Ci sono tutte le chiavi in ogni sezione.")
 
-        self.logger.info("Controllo che tutte le opzioni abbiano valori accettabli...")
+        self.__logger.info("Controllo che tutte le opzioni abbiano valori accettabli...")
         # In realtà bisognerebbe controllare se il path del database sia valido, ma vbb, lo farò un'altra volta
         # TODO: Implementare controllo per la validità del path del database
-        self.logger.info("Tutte le opzioni hanno valori accettabili.")
+        self.__logger.info("Tutte le opzioni hanno valori accettabili.")
 
         return True
 
     @classmethod
-    def _as_dict(cls, dicts):
+    def __as_dict(cls, dicts):
         sections_dict = {}
 
         # get all defaults
