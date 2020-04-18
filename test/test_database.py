e6138373e76358c58ec477f57a3176512ecea542
territory91 <terribile_91@hotmail.it>
2020-04-18 19:01:15 +0200

    Added more test for the database

--- test/test_database.py
+++ test/test_database.py
@@ -165,6 +165,204 @@ def test_database_user_id_exist():
     assert us_exist
 
 
+def test_database_check_number_subscribtions():
+
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    num_sub = database.check_number_subscribtions(6551474276)
+
+    assert num_sub["user_id"] == 6551474276
+
+    assert num_sub["actual_registrations"] == 0
+
+
+SUB_BY_POST_LINK = {"social": "instagram",
+                    "username": "il_post",
+                    "internal_id": None,
+                    "social_id": None,
+                    "link": "www.instagram.com/il_post",
+                    "data": {},
+                    "subStatus": "social_id_not_present"
+                    }
+
+SUB_BY_POST_LINK2 = {"social": "instagram",
+                     "username": "il_post",
+                     "internal_id": "1769583068",
+                     "social_id": None,
+                     "link": "www.instagram.com/il_post",
+                     "data": {},
+                     "subStatus": "subscribable",
+                     "title": "il_post",
+                     "status": "public"
+                     }
+
+
+def test_database_get_first_social_id_from_internal_user_social_and_if_present_subscribe1():
+    '''
+        Tentativo di inscrizione ad un social tramite link senza forzarne
+        l'inserimento nel database del social e relazione con l'utente
+            => manca il social nel database
+            ==> non possiamo inserire la relazione (non forzato)
+            ===> dovremo forzare l'inserimento di entrambi
+    '''
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    mresult = database.get_first_social_id_from_internal_user_social_and_if_present_subscribe(6551474276, SUB_BY_POST_LINK, False)
+
+    assert mresult['subStatus'] == 'notInDatabase'
+
+
+def test_database_get_first_social_id_from_internal_user_social_and_if_present_subscribe2():
+    '''
+        tentativo di inscrizione ad un social tramite link forzando
+        l'inserimento del social e aggungengo la relazione con l'utente
+            => manca il social nel database
+            ==> forziamo nel database il social e la relazione con l'utente
+    '''
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    mresult = database.get_first_social_id_from_internal_user_social_and_if_present_subscribe(6551474276, SUB_BY_POST_LINK2, True)
+    assert mresult['social_id'] == 1
+    assert mresult['subStatus'] == 'CreatedSocialAccAndSubscribed'
+
+
+def test_database_get_first_social_id_from_internal_user_social_and_if_present_subscribe3():
+    '''
+        Tentativo di inscrizione ad un social tramite link senza forzarne
+        l'inserimento nel database del social e relazione con l'utente
+            => il social è presente nel database
+            ==> estraggo le informazioni del social dal database
+            ===> aggungiamo la relazione tra l'utente e il social
+    '''
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    mresult = database.get_first_social_id_from_internal_user_social_and_if_present_subscribe(57356765765, SUB_BY_POST_LINK, False)
+    assert mresult['internal_id'] == '1769583068'
+    assert mresult['social_id'] == 1
+    assert mresult['subStatus'] == 'JustSubscribed'
+
+
+def test_database_get_first_social_id_from_internal_user_social_and_if_present_subscribe4():
+    '''
+        Tentativo di ri-inscrizione ad un social
+    '''
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    mresult = database.get_first_social_id_from_internal_user_social_and_if_present_subscribe(6551474276, SUB_BY_POST_LINK, False)
+
+    assert mresult['subStatus'] == 'AlreadySubscribed'
+
+
+def test_database_create_dict_of_user_ids_and_socials():
+
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    res = database.create_dict_of_user_ids_and_socials
+
+    il_post = {"internal_id": "1769583068",
+               "username": "il_post",
+               "title": "il_post",
+               "retreive_time": "1587222607",
+               "status": "public"
+               }
+
+    # Il retreive_time cambia ogni volta che si lanciano i test,
+    # dato che ad ogni lancio dei test il database viene resettato
+
+    assert res['social_accounts']['instagram'][0]['internal_id'] == il_post['internal_id']
+    assert res['social_accounts']['instagram'][0]['username'] == il_post['username']
+    assert res['social_accounts']['instagram'][0]['title'] == il_post['title']
+    assert res['social_accounts']['instagram'][0]['status'] == il_post['status']
+
+    assert res['subscriptions']['instagram'][il_post['internal_id']] == ['6551474276', '57356765765']
+
+
+QUERY_TODO_UPDATE = {"update": [{"type": "retreive_time",
+                                 "social": "instagram",
+                                 "internal_id": "1769583068",
+                                 "retreive_time": "999999999"
+                                 },
+                                {"type": "status",
+                                 "social": "instagram",
+                                 "internal_id": "1769583068",
+                                 "status": "private"
+                                 }
+                                ],
+                     "delete": []
+                     }
+
+
+def test_database_process_messages_queries1():
+
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    database.process_messages_queries(QUERY_TODO_UPDATE)
+
+    res, _ = myquery(DATABASE_PATH,
+                     "SELECT retreive_time, status FROM socials WHERE social = ? AND internal_id = ?",
+                     'instagram', 1769583068)
+
+    assert res[0] == 999999999
+    assert res[1] == 'private'
+
+
+QUERY_TODO_DELETE = {"delete": [{"type": "socialAccount",
+                                 "social": "instagram",
+                                 "internal_id": "1769583068"
+                                 }
+                                ],
+                     "update": []
+                     }
+
+
+def test_database_process_messages_queries2():
+
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    database.process_messages_queries(QUERY_TODO_DELETE)
+
+    res, _ = myquery(DATABASE_PATH, "SELECT count() FROM registrations")
+
+    assert res[0] == 0
+
+    res, _ = myquery(DATABASE_PATH, "SELECT count() FROM socials")
+
+    assert res[0] == 0
+
+
+def test_database_clean_dead_subscriptions():
+
+    assert path.exists(DATABASE_PATH)
+    database = MyDatabase(DATABASE_PATH)
+
+    # creo una sottoscrizione ad un social di un utente fintizzio
+    _ = database.get_first_social_id_from_internal_user_social_and_if_present_subscribe(1234567890, SUB_BY_POST_LINK2, True)
+
+    # rimuovo la relazione tra l'utente e il social
+    myquery(DATABASE_PATH, "DELETE FROM registrations WHERE user_id = ?", 1234567890)
+    # verifico che sia stata rimossa
+    res, _ = myquery(DATABASE_PATH, "SELECT count() FROM registrations")
+    assert res[0] == 0
+
+    # verifico che il social sia ancora presente nel database
+    res, _ = myquery(DATABASE_PATH, "SELECT count() FROM socials")
+    assert res[0] == 1
+
+    database.clean_dead_subscriptions()
+
+    # verifico che il social sia stato rimosso dal database perchè nessuno è inscritto
+    res, _ = myquery(DATABASE_PATH, "SELECT count() FROM socials")
+    assert res[0] == 0
+
+
 def test_database_unsubscribe():
 
     # Mi assicuro che il file non esista/ sia stato cancellato
