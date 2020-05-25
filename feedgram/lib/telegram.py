5d70bc246b8fccb4628ac8cb5fc2d1443fc8afab
territory91 <terribile_91@hotmail.it>
2020-05-25 20:14:15 +0200

    Added telegram faature for set the list of the bot's commands
    https://core.telegram.org/bots/api#setmycommands

--- feedgram/lib/telegram.py
+++ feedgram/lib/telegram.py
@@ -23,6 +23,38 @@ class Telegram:
         self.__logger.info('Class instance for communication with Telegram\'s bot APIs initiated.')
         self.__tl_url = "https://api.telegram.org/bot{}/".format(token)
 
+    def check_comand_info(self):
+        comands = [{'command': 'start', 'description': 'Start the bot and subscribe'},
+
+                   {'command': 'help', 'description': 'Get information abouth the Bot'},
+                   {'command': 'sub', 'description': 'Follow social account'},
+                   {'command': 'unsub', 'description': 'Stop to follow social account'},
+                   {'command': 'list', 'description': 'List your subscrition'},
+
+                   {'command': 'mute', 'description': 'Receive news without sound'},
+                   {'command': 'halt', 'description': 'Stop to receive news'},
+                   {'command': 'pause', 'description': 'Pause the news for retrieve later'},
+
+                   {'command': 'stop', 'description': 'Stop the bot and unsubscribe'}
+                   ]
+
+        actual_command = self.__get_my_commands()["result"]
+        if comands != actual_command:
+            self.__set_my_comands(comands)
+
+    def __get_my_commands(self):
+        url = self.__tl_url + "getMyCommands"
+        return self.__get_json_from_url(url)
+
+    def __set_my_comands(self, comands):
+        url = self.__tl_url + "setMyCommands"
+        url = url + "?commands={}".format(quote_plus(json.dumps(comands)))
+        get_url(url)
+        # [{'command': 'start', 'description': 'Start the bot'}, {'command': 'stop', 'description': 'Stop the bot'}]
+
+
+
+
     # TODO: Mettere nel config il timeout in modo che possa esse impostato manualmente dall'utente
     def get_updates(self, offset=None):
         url = self.__tl_url + "getUpdates?timeout=60"
